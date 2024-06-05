bfa152908742fa1ddf2825d0ec7f4f0fa8f7273c
2acholsk1 <p.zacholski@wp.pl>
2024-06-05 12:26:33 +0200

    adding main
    Co-authored-by: MaciejBr01 <MaciejBr01@users.noreply.github.com>

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+from pathlib import Path
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_dir', type=str)
-    args = parser.parse_args()
+
+
+def compute_prob(images_path, bb_path):
+    pass
+    
 
     
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_dir', type=str)
+    args = parser.parse_args()
+
+    data_dir = Path(args.data_dir)
+    images_dir = Path(os.path.join(str(data_dir), 'frames'))
+    bb_dir = Path(os.path.join(str(data_dir), 'bboxes.txt'))
+    
+    images_path = sorted([img_path for img_path in images_dir.iterdir() if img_path.name.endswith('.jpg')])
+
+    file = open(bb_dir, 'r')
+    
+    compute_prob()
+
